c7bfeee47f71281e2cc6ba290791444262bef5ed
Nícolas Bezerra <nicolas09br@gmail.com>
2026-08-17 19:17:04 -0300

    exercício 5

---
+++ 005.py
@@ -0,0 +1,8 @@
+#Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+numerolegal = int(input('Digite um número: '))
+sucessor = numerolegal + 1
+antecessor = numerolegal - 1
+print('Numero escolhido: {}'.format(numerolegal))
+print('Sucessor do numero: {}'.format(sucessor))
+print('Antecessor do número: {}'.format(antecessor))
