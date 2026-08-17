4bd3f95c3266153b77cdcc345cb5dc20f7a39241
Nícolas Bezerra <nicolas09br@gmail.com>
2026-08-17 19:35:43 -0300

    exercício 7

---
+++ 007.py
@@ -0,0 +1,6 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+n1 = float(input('Digite uma nota do aluno: '))
+n2 = float(input('Digite outra nota do aluno: '))
+nM = (n1 + n2) / 2
+print('A média das duas notas do aluno é: {}'.format(nM))
