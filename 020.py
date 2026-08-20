ffe32cb180b77e737822619864a037a119da44ce
Nícolas Bezerra <nicolas09br@gmail.com>
2026-08-19 22:11:06 -0300

    exercício 20, embaralhar lista

---
+++ 020.py
@@ -0,0 +1,12 @@
+#O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+import random
+
+n1 = input('Digite o primeiro nome: ')
+n2 = input('Digite o segundo nome: ')
+n3 = input('Digite o terceiro nome: ')
+n4 = input('Digite o quarto nome: ')
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+print('A ordem de apresentação será:')
+print(lista)
